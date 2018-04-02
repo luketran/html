193f8d2648cd7f4f7dce4d9ba5d80fe19dc5874e
Admin <phuoctran1192@gmail.com>
2018-04-02 17:48:10 +0700

    thay doi css

--- theme1/theme1.js
+++ theme1/theme1.js
@@ -3,24 +3,32 @@ $(document).ready(function() {
 		event.preventDefault();
 		/* Act on the event */
 		
-		$('html,body').animate({scrollTop: $('.chapter1').offset().top}, 400);
+		$('html,body').animate({scrollTop: $('.chapter1').offset().top}, 1000,"easeOutBack");
 	});
 	$('.menutren ul li:nth-child(2) a').on('click', function(event) {
 		event.preventDefault();
 		/* Act on the event */
 		
-		$('html,body').animate({scrollTop: $('.chapter2').offset().top}, 400);
+		$('html,body').animate({scrollTop: $('.chapter2').offset().top}, 1000,"easeOutBack");
 	});
 	$('.menutren ul li:nth-child(3) a').on('click', function(event) {
 		event.preventDefault();
 		/* Act on the event */
 		
-		$('html,body').animate({scrollTop: $('.chapter3').offset().top}, 400);
+		$('html,body').animate({scrollTop: $('.chapter3').offset().top}, 1000,"easeOutBack");
 	});
 	$('.menutren ul li:nth-child(4) a').on('click', function(event) {
 		event.preventDefault();
 		/* Act on the event */
 		
-		$('html,body').animate({scrollTop: $('.chapter4').offset().top}, 400);
+		$('html,body').animate({scrollTop: $('.chapter4').offset().top}, 1000,"easeOutBack");
+	});
+
+	//back to top
+	$('.backtotop').on('click', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		
+		$('html,body').animate({scrollTop: 0}, 400);
 	});
 });
