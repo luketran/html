603b337eda68d256d73b976bbede502e3d274bc2
Admin <phuoctran1192@gmail.com>
2018-04-02 15:47:20 +0700

    code xong phan dich chuyen

--- theme1/theme1.js
+++ theme1/theme1.js
@@ -1,8 +1,26 @@
-$(function() {
+$(document).ready(function() {
 	$('.menutren ul li:nth-child(1) a').on('click', function(event) {
 		event.preventDefault();
 		/* Act on the event */
-		console.log('1');
-		$('body').animate({scrollTop:900}, 1000);
+		
+		$('html,body').animate({scrollTop: $('.chapter1').offset().top}, 400);
+	});
+	$('.menutren ul li:nth-child(2) a').on('click', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		
+		$('html,body').animate({scrollTop: $('.chapter2').offset().top}, 400);
+	});
+	$('.menutren ul li:nth-child(3) a').on('click', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		
+		$('html,body').animate({scrollTop: $('.chapter3').offset().top}, 400);
+	});
+	$('.menutren ul li:nth-child(4) a').on('click', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		
+		$('html,body').animate({scrollTop: $('.chapter4').offset().top}, 400);
 	});
 });
