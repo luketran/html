cef9422113427c55a4a057a8b9230b5fb6e48141
luketran <phuoctran1192@gmail.com>
2018-04-06 16:53:15 +0700

    xoa 2 file test

--- theme2/1.js
+++
@@ -1,9 +0,0 @@
-﻿ $(function(){
- 	$('.menutren ul li').on('click', function(event) {
- 		event.preventDefault();
- 		/* Act on the event */
- 		console.log('123123');
- 		$('html,body').animate({scrollTop:500}, 1000);
- 	});
-})  
- 
