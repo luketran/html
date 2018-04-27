0e8913fd5529c02f8995a3f63a7bf9eb40c4a46e
luketran <phuoctran1192@gmail.com>
2018-04-27 23:03:57 +0700

    them du an lime orange

---
+++ baitest/1.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function() {
+	$('.scroll img').on('click', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		// console.log('123123');
+		$('html,body').animate({scrollTop: $('.content2').offset().top}, 500);
+	});
+	$('.airun').animateRotate(90, {
+	  duration: 1337,
+	  easing: 'linear',
+	  complete: function () {},
+	  step: function () {}
+	});
+	
+});
